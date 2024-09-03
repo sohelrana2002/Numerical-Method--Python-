bf5f8e6c2e62b20c7fab33f9ffb8ab524f4ecbcc
SOHEL RANA <sohelrana104322@gmail.com>
2024-09-03 12:35:35 +0600

    add initial files & folders

---
+++ Bisection Method/bisection.py
@@ -0,0 +1,34 @@
+# =====f(x)= x**3 - x**2 - 2========
+
+def f(x):
+    return x**3 - x**2 - 2
+
+
+def bisection_method(f, a, b, tol):
+    #code here
+    if f(a) * f(b) >= 0:
+        print("Bisection method fails")
+        return None
+    
+    c = a
+
+    while abs(0 - f(c)) >= tol:
+        c = (a+b)/2
+
+        if f(c) == 0:
+            break
+
+        if f(c) * f(b) < 0:
+            a = c
+
+        else:
+            b = c
+    
+    return c
+
+
+#diver code
+z = bisection_method(f, 1.5, 2, 1e-6)
+c = f(z)
+print(c)
+print(f"{c: .4f}")
